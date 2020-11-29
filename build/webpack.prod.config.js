d44b05b937975eceb01b7ee5163fcf587fea7b7e
Woshiajuana <zhigang.chen@owulia.com>
2020-11-29 15:07:18 +0800

    subcode

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,14 +1,11 @@
 
 const { merge } = require('webpack-merge');
 
-
-const webpackBaseConfig = require('./webpack.base.config');
 const webpackDevConfig = require('./webpack.dev.config');
 
 module.exports = merge(webpackDevConfig, {
 
     // 模式
     mode: 'production',
-
 
 });
